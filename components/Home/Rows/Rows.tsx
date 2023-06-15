e1f6719a8e409200a35a20ed3c953826bcbb53e1
arihanv <arihanvaranasi@gmail.com>
2023-06-14 23:51:11 -0500

    Created Title Card

--- components/Home/Rows/Rows.tsx
+++ components/Home/Rows/Rows.tsx
@@ -11,24 +11,24 @@ const Rows: React.FC = () => {
         <div className="image-container">
           <img className="brightness-[0.7]" src={hills.src} alt="" />
           <span className="image-text hover:backdrop-blur-sm transition duration-500">
-          <div className="backdrop-blur-sm text-3xl font-[450] tracking-tight p-5 rounded-xl">
-           My Carbon Footprint
+            <div className="backdrop-blur-sm text-3xl font-[450] tracking-tight p-5 rounded-xl">
+              My Carbon Footprint
             </div>
           </span>
         </div>
         <div className="image-container bg-green-500">
           <img className="brightness-[0.7]" src={leaf.src} alt="" />
           <span className="image-text hover:backdrop-blur-sm transition duration-500">
-          <div className="backdrop-blur-sm text-3xl font-[450] tracking-tight p-5 rounded-xl">
-            AI Analysis
+            <div className="backdrop-blur-sm text-3xl font-[450] tracking-tight p-5 rounded-xl">
+              AI Analysis
             </div>
           </span>
         </div>
         <div className="image-container">
           <img className="brightness-[0.7]" src={galaxy.src} alt="" />
           <span className="image-text hover:backdrop-blur-sm transition duration-500">
-          <div className="backdrop-blur-sm text-3xl font-[450] tracking-tight p-5 rounded-xl">
-            Badges
+            <div className="backdrop-blur-sm text-3xl font-[450] tracking-tight p-5 rounded-xl">
+              Badges
             </div>
           </span>
         </div>
@@ -36,7 +36,7 @@ const Rows: React.FC = () => {
           <img className="brightness-[0.7]" src={trees.src} alt="" />
           <span className="image-text hover:backdrop-blur-sm transition duration-500">
             <div className="backdrop-blur-sm text-3xl font-[450] tracking-tight p-5 rounded-xl">
-            Blog Zone
+              Blog Zone
             </div>
           </span>
         </div>
