a7bfc1a764be35435f1aa99626439c024bfc4307
Akshay-Vs <akshay.vs2005@gmail.com>
2023-06-15 11:31:14 +0530

    Image Optimization

--- components/Home/Rows/Rows.tsx
+++ components/Home/Rows/Rows.tsx
@@ -3,13 +3,24 @@ import hills from "./images/hills.jpg";
 import trees from "./images/trees.jpg";
 import galaxy from "./images/galaxy.jpg";
 import leaf from "./images/leaf.jpg";
+import Image from "next/image";
+
 import "./Rows.scss";
 const Rows: React.FC = () => {
   return (
     <>
       <div className="card grid grid-rows-4 gap-3">
         <div className="image-container">
-          <img className="brightness-[0.7]" src={hills.src} alt="" />
+          <div style={{ width: "100%" }}>
+            <Image
+              className="brightness-[0.7]"
+              src={hills.src}
+              alt=""
+              width={800}
+              height={600}
+            />
+          </div>
+
           <span className="image-text hover:backdrop-blur-sm transition duration-500">
             <div className="backdrop-blur-sm text-3xl font-[450] tracking-tight p-5 rounded-xl">
               My Carbon Footprint
@@ -17,7 +28,13 @@ const Rows: React.FC = () => {
           </span>
         </div>
         <div className="image-container bg-green-500">
-          <img className="brightness-[0.7]" src={leaf.src} alt="" />
+          <Image
+            className="brightness-[0.7]"
+            src={leaf.src}
+            alt=""
+            width={800}
+            height={600}
+          />
           <span className="image-text hover:backdrop-blur-sm transition duration-500">
             <div className="backdrop-blur-sm text-3xl font-[450] tracking-tight p-5 rounded-xl">
               AI Analysis
@@ -25,7 +42,13 @@ const Rows: React.FC = () => {
           </span>
         </div>
         <div className="image-container">
-          <img className="brightness-[0.7]" src={galaxy.src} alt="" />
+          <Image
+            className="brightness-[0.7]"
+            src={galaxy.src}
+            alt=""
+            width={800}
+            height={600}
+          />
           <span className="image-text hover:backdrop-blur-sm transition duration-500">
             <div className="backdrop-blur-sm text-3xl font-[450] tracking-tight p-5 rounded-xl">
               Badges
@@ -33,7 +56,13 @@ const Rows: React.FC = () => {
           </span>
         </div>
         <div className="image-container">
-          <img className="brightness-[0.7]" src={trees.src} alt="" />
+          <Image
+            className="brightness-[0.7]"
+            src={trees.src}
+            alt=""
+            width={800}
+            height={600}
+          />
           <span className="image-text hover:backdrop-blur-sm transition duration-500">
             <div className="backdrop-blur-sm text-3xl font-[450] tracking-tight p-5 rounded-xl">
               Blog Zone
