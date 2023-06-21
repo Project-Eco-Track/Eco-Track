f3b252597c3464991835794b804cd4813a12201c
Akshay-Vs <akshay.vs2005@gmail.com>
2023-06-21 10:46:01 +0530

    Optimization

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import Blog from '@components/Blog'
+import React from "react";
+import Blog from "@components/Blog";
+import { Suspense } from "react";
 const page = () => {
   return (
     <div>
-      <Blog />
+      <Suspense fallback={<h1 className="text-white">Loading...</h1>}>
+        <Blog />
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
