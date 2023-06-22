a456e4adfba012ba1a84be564015ddbea4182e9e
arihanv <arihanvaranasi@gmail.com>
2023-06-22 16:40:41 -0500

    Post User ID to Backend

--- middleware.js
+++ middleware.js
@@ -1,6 +1,21 @@
 import { authMiddleware } from "@clerk/nextjs"
+import axios from 'axios';
 
+async function makeRequest(userid) {
+  try {
+    const response = await axios.post("/api/user/login", { userid });
+    console.log('Response:', response.data);
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+}
 export default authMiddleware({
+  async afterAuth(auth, req, evt) {
+    if (auth.userId) {
+      console.log("User ID:", auth.userId)
+      await makeRequest(auth.userId)
+    }
+  },
   publicRoutes: ["/"],
 })
 
