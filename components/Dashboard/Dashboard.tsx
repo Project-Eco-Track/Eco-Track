c4b25aecd7f7cbabaf752851509f6202caab3d03
arihanv <arihanvaranasi@gmail.com>
2023-06-21 23:53:34 -0500

    Updates to Badge Dashboard

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TopBar from "./TopBar";
 import BadgeCard from "./BadgeCard";
+import { BadgeCardSkeleton } from "./BadgeCard/BadgeCard";
 
 const Dashboard: React.FC = () => {
   return (
@@ -8,9 +9,18 @@ const Dashboard: React.FC = () => {
       <div className="text-white flex justify-center p-10">
         <TopBar />
       </div>
-      <div className="w-full text-white flex flex-wrap gap-10">
-        <BadgeCard img="https://png.pngtree.com/png-vector/20220730/ourmid/pngtree-green-eco-friendly-logo-symbol-design-png-image_6093132.png" starNum={2} desc={"Earned for planting 5 trees"} title={"Tree Planter"} />
-       
+      <div className="text-white flex flex-wrap gap-10 px-20">
+        <BadgeCard
+          img="https://png.pngtree.com/png-vector/20220730/ourmid/pngtree-green-eco-friendly-logo-symbol-design-png-image_6093132.png"
+          starNum={2}
+          desc={"Earned for planting 5 trees"}
+          title={"Tree Planter"}
+        />
+        <BadgeCardSkeleton/>
+        <BadgeCardSkeleton/>
+        <BadgeCardSkeleton/>
+        <BadgeCardSkeleton/>
+        <BadgeCardSkeleton/>
       </div>
     </>
   );
