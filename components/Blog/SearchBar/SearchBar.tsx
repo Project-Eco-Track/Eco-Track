9320206f2e8326aeb1242d2da148d9bbfedb0e8b
Akshay-Vs <akshay.vs2005@gmail.com>
2023-07-19 17:31:10 +0530

    Added mobile UI

--- components/Blog/SearchBar/SearchBar.tsx
+++ components/Blog/SearchBar/SearchBar.tsx
@@ -13,44 +13,44 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="searchbar">
-      <div className="searchbar-container">
-        <div className="searchbar-left">
-          <div
-            className={`button ${selected === "Top Picks" ? "selected" : ""}`}
-            onClick={() => handleFilter("Top Picks")}
-          >
-            Top Picks
+      <div className="searchbar">
+        <div className="searchbar-container">
+          <div className="searchbar-left">
+            <div
+              className={`button ${selected === "Top Picks" ? "selected" : ""}`}
+              onClick={() => handleFilter("Top Picks")}
+            >
+              Top Picks
+            </div>
+            <div
+              className={`button ${selected === "Recent" ? "selected" : ""}`}
+              onClick={() => handleFilter("Recent")}
+            >
+              Recent
+            </div>
+            <div
+              className={`button ${selected === "Popular" ? "selected" : ""}`}
+              onClick={() => handleFilter("Popular")}
+            >
+              Popular
+            </div>
+            <div
+              className={`button ${
+                selected === "Eco Verified" ? "selected" : ""
+              }`}
+              onClick={() => handleFilter("Eco Verified")}
+            >
+              Eco Verified
+            </div>
           </div>
-          <div
-            className={`button ${selected === "Recent" ? "selected" : ""}`}
-            onClick={() => handleFilter("Recent")}
-          >
-            Recent
-          </div>
-          <div
-            className={`button ${selected === "Popular" ? "selected" : ""}`}
-            onClick={() => handleFilter("Popular")}
-          >
-            Popular
-          </div>
-          <div
-            className={`button ${
-              selected === "Eco Verified" ? "selected" : ""
-            }`}
-            onClick={() => handleFilter("Eco Verified")}
-          >
-            Eco Verified
-          </div>
-        </div>
-        <div className="searchbar-right">
-          <div className="searchbar-search">
-            <input type="text" placeholder="Search for blogs" />
-            <i className="fa-solid fa-magnifying-glass p-5 fa-lg"></i>
+          <div className="searchbar-right">
+            <div className="searchbar-search">
+              <input type="text" placeholder="Search for blogs" />
+              <i className="fa-solid fa-magnifying-glass p-5 fa-lg"></i>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
