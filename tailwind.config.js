3a9c167b4bde819bbfb236c18884c4a682a2711d
Akshay-Vs <akshay.vs2005@gmail.com>
2023-07-25 08:44:44 +0530

    Removed light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,38 +17,37 @@ module.exports = {
       },
       colors: {
         // light mode
-        tremor: {
-          brand: {
-            faint: "#eff6ff", // blue-50
-            muted: "#bfdbfe", // blue-200
-            subtle: "#60a5fa", // blue-400
-            DEFAULT: "#3b82f6", // blue-500
-            emphasis: "#1d4ed8", // blue-700
-            inverted: "#ffffff", // white
-          },
-          background: {
-            muted: "#f9fafb", // gray-50
-            subtle: "#f3f4f6", // gray-100
-            DEFAULT: "#ffffff", // white
-            emphasis: "#374151", // gray-700
-          },
-          border: {
-            DEFAULT: "#e5e7eb", // gray-200
-          },
-          ring: {
-            DEFAULT: "#e5e7eb", // gray-200
-          },
-          content: {
-            subtle: "#9ca3af", // gray-400
-            DEFAULT: "#6b7280", // gray-500
-            emphasis: "#374151", // gray-700
-            strong: "#111827", // gray-900
-            inverted: "#ffffff", // white
-          },
-        },
-        
+        // tremor: {
+        //   brand: {
+        //     faint: "#eff6ff", // blue-50
+        //     muted: "#bfdbfe", // blue-200
+        //     subtle: "#60a5fa", // blue-400
+        //     DEFAULT: "#3b82f6", // blue-500
+        //     emphasis: "#1d4ed8", // blue-700
+        //     inverted: "#ffffff", // white
+        //   },
+        //   background: {
+        //     muted: "#f9fafb", // gray-50
+        //     subtle: "#f3f4f6", // gray-100
+        //     DEFAULT: "#ffffff", // white
+        //     emphasis: "#374151", // gray-700
+        //   },
+        //   border: {
+        //     DEFAULT: "#e5e7eb", // gray-200
+        //   },
+        //   ring: {
+        //     DEFAULT: "#e5e7eb", // gray-200
+        //   },
+        //   content: {
+        //     subtle: "#9ca3af", // gray-400
+        //     DEFAULT: "#6b7280", // gray-500
+        //     emphasis: "#374151", // gray-700
+        //     strong: "#111827", // gray-900
+        //     inverted: "#ffffff", // white
+        //   },
+        // },
         // dark mode
-        "dark mode": {
+        tremor: {
           brand: {
             faint: "#0B1229", // custom
             muted: "#172554", // blue-950
