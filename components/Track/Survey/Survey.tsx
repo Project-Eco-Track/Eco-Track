3b8c71b2ef4a9a6cce2104a51777c9a3d5200ded
Akshay-Vs <akshay.vs2005@gmail.com>
2023-07-27 11:22:15 +0530

    Fixed form submission

--- components/Track/Survey/Survey.tsx
+++ components/Track/Survey/Survey.tsx
@@ -19,12 +19,10 @@ const TakeSurvey = () => {
   const survey = new Model(json);
   survey.onComplete.add((sender, options) => {
     const result = JSON.stringify(sender.data, null, 3);
-    alert(result);
     if (isSignedIn && isLoaded) {
       postCarbonFootprint(
         result.replace(/\\|\n/g , ''),
         user.id,
-        user.fullName
       );
     }
   });
--- libs/postCarbonFootprint.ts
+++ libs/postCarbonFootprint.ts
@@ -1,15 +1,11 @@
 // type: Library
 import { postRequest } from "./postRequest";
+import { formateCF } from "@utils/formateCF";
 
-const postCarbonFootprint = async (
-  data: any,
-  userId: string,
-  userName: any
-) => {
-  const url = '/api/carbonFootprint';
-  const payload = {data, userId, userName };
-
-  console.log(payload);
+const postCarbonFootprint = async (data: any, userId: string) => {
+  const url = "https://sangria-swordfish-wrap.cyclic.app/post/carbon-footprint";
+  const json = await formateCF(JSON.parse(data));
+  const payload = { ...json }; //userId
   const res = postRequest<any>(`${url}`, payload);
   return JSON.stringify(res);
 };
